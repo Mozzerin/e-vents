f59be4e6a1fdb65b143239864fcb2578c008ef26
Mozzerin <mozzerin@gmail.com>
2016-12-31 15:14:28 +0300

    add daily rotate logging to file

--- libs/log.js
+++ libs/log.js
@@ -2,6 +2,7 @@
  * Created by Mozzerin on 29.12.2016.
  */
 var winston = require('winston');
+require('winston-daily-rotate-file');
 
 function getLogger(module) {
     var path = module.filename.split('/').slice(-2).join('/'); //отобразим метку с именем файла, который выводит сообщение
@@ -12,6 +13,12 @@ function getLogger(module) {
                 colorize:   true,
                 level:      'debug',
                 label:      path
+            }),
+            new winston.transports.DailyRotateFile({
+                filename: '../e-events_api.log',
+                datePattern: 'yyyy-MM-dd.',
+                prepend: true,
+                level: 'debug'
             })
         ]
     });
